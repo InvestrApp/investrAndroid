b2de44540f8efb66dad78b7d511481485dec78fe
Sahil Agarwal <sahil.agarwal@xoom.com>
2017-10-13 13:07:25 -0700

    Added toolbar and a navigation view to switch through fragments

---
+++ app/src/main/java/com/investrapp/investr/models/Competition.java
@@ -0,0 +1,9 @@
+package com.investrapp.investr.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("Competition")
+public class Competition extends ParseObject {
+
+}
