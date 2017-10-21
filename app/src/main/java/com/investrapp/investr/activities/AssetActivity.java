1aea46187c94b375fda91b267a34eb30c88dbfca
Michael Signorotti <michaelsignorotti@Michaels-MacBook-Pro.local>
2017-10-20 23:44:00 -0700

    Added current price to AssetActivity

--- app/src/main/java/com/investrapp/investr/activities/AssetActivity.java
+++ app/src/main/java/com/investrapp/investr/activities/AssetActivity.java
@@ -2,7 +2,6 @@ package com.investrapp.investr.activities;
 
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,18 +11,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.investrapp.investr.R;
+import com.investrapp.investr.apis.AlphaVantageClient;
+import com.investrapp.investr.apis.handlers.AlphaVantageDigitalCurrencyCurrentPriceCallHandler;
 import com.investrapp.investr.fragments.AssetPriceFragment;
 import com.investrapp.investr.fragments.PricesPagerAdapter;
-import com.investrapp.investr.models.Asset;
 import com.investrapp.investr.models.CryptocurrencyPriceTimeSeries;
-import com.investrapp.investr.models.Price;
-
-import org.parceler.Parcels;
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-
-import static android.R.attr.timeZone;
 
 
 public class AssetActivity extends AppCompatActivity implements AssetPriceFragment.OnPriceTimeSeriesResponseListener {
@@ -73,6 +65,8 @@ public class AssetActivity extends AppCompatActivity implements AssetPriceFragme
         Intent intent = getIntent();
         String ticker = intent.getStringExtra("ticker");
 
+        getAssetPrice(ticker);
+
         pricesPagerAdapter = new PricesPagerAdapter(getSupportFragmentManager(), this, ticker);
         vpPager.setAdapter(pricesPagerAdapter);
 
@@ -98,5 +92,24 @@ public class AssetActivity extends AppCompatActivity implements AssetPriceFragme
         tvMarketName.setText(marketName);
         tvLastRefreshed.setText(lastRefreshed);
         tvTimeZone.setText(timeZone);
+    }
+
+    public void getAssetPrice(String assetTicker) {
+
+        AlphaVantageClient.getCurrentDigitalCurrencyPrice(assetTicker, new AlphaVantageDigitalCurrencyCurrentPriceCallHandler() {
+            @Override
+            public void onPriceResponse(Double price) {
+                Log.d("AssetActivity", "current price:  " + price);
+                setAssetPrice(price);
+            }
+        });
+    }
+
+    public void setAssetPrice(final Double price) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                tvAssetPrice.setText("" + String.format ("%,.2f", price));
+            }
+        });
     }
 }
